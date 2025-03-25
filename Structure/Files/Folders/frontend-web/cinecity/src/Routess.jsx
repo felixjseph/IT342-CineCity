a3f5777e4a45734b1ccb9e2f30299693e7ac134b
Al Prince Llavan <alprincellavan2019@gmail.com>
2025-03-25 19:46:54 +0800

    adding functionality to signup

--- Structure/Files/Folders/frontend-web/cinecity/src/Routess.jsx
+++ Structure/Files/Folders/frontend-web/cinecity/src/Routess.jsx
@@ -1,12 +1,20 @@
 import { Router,Routes, Route,Navigate } from "react-router-dom"
 import Login from './pages/Login and Register/Login'
 import Signup from "./pages/Login and Register/Signup"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Movies from "./pages/Admin/Movies/Movies"
 
 export default function Routess(){
     return(
         <Routes>
             <Route path="/login" element={<Login/>}></Route>
             <Route path="/signup" element={<Signup/>}/>
+            <Route path="/" element={<Navbar/>}>
+                <Route index element={<Navigate to="login"/>}></Route>
+                <Route path="home" element={<Home/>}></Route>
+            </Route>
+            <Route path="/admin/movies" element={<Movies/>}></Route>
         </Routes>
     )
 }
---
+++ Structure/Files/Folders/frontend-web/cinecity/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="border-b-2 border-opacity-30 border-gray-200/30 px-16 py-2">
+                <div className="flex">
+                    <img src="/images/logo.png" alt="logo" className="w-[10rem]" />
+                    <ul className="text-white flex items-center">
+                        <li className="mx-4 cursor-pointer relative after:content-[''] after:block after:w-0 after:h-[2px] after:bg-green-500 after:transition-all after:duration-300 hover:after:w-full">HOME</li>
+                        <li className="mx-4 cursor-pointer relative after:content-[''] after:block after:w-0 after:h-[2px] after:bg-green-500 after:transition-all after:duration-300 hover:after:w-full">MOVIES</li>
+                        <li className="mx-4 cursor-pointer relative after:content-[''] after:block after:w-0 after:h-[2px] after:bg-green-500 after:transition-all after:duration-300 hover:after:w-full">PROFILE</li>
+                    </ul>
+                </div>
+            </nav>
+            <Outlet></Outlet>
+        </div>
+    )
+}
