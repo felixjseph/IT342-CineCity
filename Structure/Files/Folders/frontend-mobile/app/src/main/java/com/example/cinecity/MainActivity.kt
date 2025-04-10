9d51a75ff359b34e75bb7b224a67039fa90e2e2a
Felix Castañeda <felixjoseph.castaneda@cit.edu>
2025-04-11 01:20:31 +0800

    Added First Page UI

--- Structure/Files/Folders/frontend-mobile/app/src/main/java/com/example/cinecity/MainActivity.kt
+++ Structure/Files/Folders/frontend-mobile/app/src/main/java/com/example/cinecity/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cinecity.ui.theme.CineCityTheme
+import com.example.cinecity.ui.screens.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             CineCityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    WelcomeScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        onSignInClick = {},
+                        onCreateAccountClick = {}
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CineCityTheme {
-        Greeting("Android")
     }
 }
