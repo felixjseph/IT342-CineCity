d0c8a70f3ea2f35dc60c644dea13614c6f0a961f
Felix Castañeda <felixjoseph.castaneda@cit.edu>
2025-04-22 11:55:09 +0800

    Added Cinecity Icon + Device IP Security Config

--- Structure/Files/Folders/frontend-mobile/app/src/main/java/com/example/cinecity/data/api/RetrofitClient.kt
+++ Structure/Files/Folders/frontend-mobile/app/src/main/java/com/example/cinecity/data/api/RetrofitClient.kt
@@ -9,9 +9,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/" // For emulator
+    private const val BASE_URL = "http://192.168.254.100:8080/"
     // "http://10.0.2.2:8080/" For emulator
-    // "http://device-server-ip:8080/" for real device
+    // "http://192.168.254.100:8080/" for real device
 
     fun getApiService(context: Context): ApiService {
         return Retrofit.Builder()
